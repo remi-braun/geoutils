5b4efe448f6ac877b1bdcdebe6bfd9c34b519825
Rémi Braun <67311115+remi-braun@users.noreply.github.com>
2025-12-10 14:24:06 +0100

    Remove version import error handling

--- geoutils/__init__.py
+++ geoutils/__init__.py
@@ -29,14 +29,3 @@ from geoutils.pointcloud import PointCloud  # noqa isort:skip
 
 # To-be-deprecated
 from geoutils.raster import Mask  # noqa isort:skip
-
-try:
-    from geoutils._version import __version__ as __version__  # noqa
-except ImportError:  # pragma: no cover
-    raise ImportError(
-        "geoutils is not properly installed. If you are "
-        "running from the source directory, please instead "
-        "create a new virtual environment (using conda or "
-        "virtualenv) and then install it in-place by running: "
-        "pip install -e ."
-    )
