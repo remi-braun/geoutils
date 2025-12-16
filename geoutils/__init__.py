1fb3cc62fbca66d28c34165308a2eabadc5bcd0f
Rémi Braun <67311115+remi-braun@users.noreply.github.com>
2025-12-16 19:58:21 +0100

    Update version format in __init__.py

--- geoutils/__init__.py
+++ geoutils/__init__.py
@@ -30,4 +30,4 @@ from geoutils.pointcloud import PointCloud  # noqa isort:skip
 # To-be-deprecated
 from geoutils.raster import Mask  # noqa isort:skip
 
-__version__ = 0.0.3.dev0
+__version__ = 0.0.3dev0
